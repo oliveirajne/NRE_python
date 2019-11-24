f82ff33857fdf1b814bf9f3ddb27b77a9e47ac84
oliveirajne <oliveirajne@gmail.com>
2019-11-24 00:47:32 -0300

    alterada msg de saída apor atingir critério de parada

--- NRE.py
+++ NRE.py
@@ -1,5 +1,4 @@
 
-
 #variaveis
 
 N, i = 0, 0
@@ -22,9 +21,9 @@ y_i = y_0
 #funcoes
 
 def funcao_1(x,y):
-	fx = x + y - 3.0e0
-	return fx
-	
+    fx = x + y - 3.0e0
+    return fx
+
 def funcao_2(x,y):
 	fx = x**2 + y**2 - 9.0e0
 	return fx
@@ -47,10 +46,7 @@ def derivada_2dy(x,y):
 	
 def TRI(A, M):
     FAC = 0
-    A = A
     TOLPIV = 1.e-11
-    M1     = M-1
-
 	
     for i in range(0, M):
         if ( abs(A[i][i]) < TOLPIV ):
@@ -82,7 +78,7 @@ def RHSUB(A, B, M, S):
 
             B[j] = B[j] - B[i]*A[j][i]/A[i][i]
 
-            S[M-1] = B[M-1]/A[M-1][M-1]
+            S[M1] = B[M1]/A[M1][M1]
 
     for i in range(0, M):
         IB = M - 1
@@ -99,38 +95,25 @@ def RHSUB(A, B, M, S):
 
 for i in range(0, N+1):
 
-    print(x_im1)
-    print(VAf_max)
-   
-    J = [[derivada_1dx(x_i,y_i),derivada_2dx(x_i,y_i)], [derivada_1dy(x_i,y_i),derivada_2dy(x_i,y_i)]] 
-	
+    J = [[derivada_1dx(x_i,y_i),derivada_2dx(x_i,y_i)], [derivada_1dy(x_i,y_i),derivada_2dy(x_i,y_i)]] 	
     F = [-funcao_1(x_i,y_i),-funcao_2(x_i,y_i)]
 
-    TRI(J, 2)
+    TRI(J, 2)  
     RHSUB(J, F, 2, S)
-
+ 
     x_im1 = S[0] + x_i
-    print("x_im1 " + "{0:16e}".format(x_im1))
-    print("S[0] " + "{0:16e}".format(S[0]))
-    print("x_i "+"{0:16e}".format(x_i))
     y_im1 = S[1] + y_i
 
     VAf_1 = abs(funcao_1(x_im1,y_im1))
 
     VAf_2 = abs(funcao_2(x_im1,y_im1))
 
-    print(S)
-    print('aha')
-    print(x_im1)
-    print(y_im1)
     if ( VAf_1 < VAf_2):
         VAf_max = VAf_2
     else:        
         VAf_max = VAf_1
-        print('MANOOOOOOOOOOOOOOOO')
-        print(VAf_1)
-        print(VAf_2)
-        
+
+       
     VAS_1 = abs(S[0])
     VAS_2 = abs(S[1])
     
@@ -149,17 +132,16 @@ for i in range(0, N+1):
 
     d_1= VAS_max 
 
-    
     if (VAXY != 0.0e0): 
         d_1=d_1/VAXY
         if (d_1 <= e):
             if (VAf_max <= e):
-                
-                print("{0:5}".format(i), "{0:16e}".format(x_im1), "{0:16e}".format(y_im1), "{0:16e}".format(VAf_max), "{0:16e}".format(d_1))   
+                print("Critério de parada atingido!")
+                # print("{0:2}".format(i), "{0:16.5e}".format(x_im1), "{0:16.5e}".format(y_im1), "{0:16.5e}".format(VAf_max), "{0:16.5e}".format(d_1))   
                 break
- 
+    
     x_i = x_im1 
     y_i = y_im1 
 
-    print("{0:5}".format(i), "{0:16e}".format(x_im1), "{0:16e}".format(y_im1), "{0:16e}".format(VAf_max), "{0:16e}".format(d_1))
-    # print(i, x_im1, y_im1, VAf_max, d_1)
+    print("{0:2}".format(i), "{0:16.5e}".format(x_im1), "{0:16.5e}".format(y_im1), "{0:16.5e}".format(VAf_max), "{0:16.5e}".format(d_1))
+
